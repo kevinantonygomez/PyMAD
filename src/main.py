d768af80c10e013bd2d6321c6db0f7c741144fb7
Kevin Gomez <kevingomez@MacBook-Pro.local>
2024-10-30 19:13:47 -0400

    Backup push before code overhaul

--- src/main.py
+++ src/main.py
@@ -1,3 +1,57 @@
+# import img_processing
+# import model
+# import numpy as np
+
+
+# def driver(): 
+#     '''
+#     When a0 is high, even the deepest valleys (or areas of low intensity) remain far from the viewpoint.
+#     When setting a1 to high values, the transformation will result in significant valleys and ridges even when the pixels that created them had similar intensity values.
+#     '''
+
+
+#     TEST_OR_TRAIN = 'train' # 'train'
+#     GOOD_UNGOOD = 'good'
+#     IMG_HEIGHT = 512
+#     IMG_WIDTH = 512
+#     A0 = 1000
+#     A1 = 1
+#     SAVE_DIR = f'data/output.nosync/Chest-RSNA/{TEST_OR_TRAIN}_{IMG_HEIGHT}_{IMG_WIDTH}_{A0}_{A1}/{GOOD_UNGOOD}' # False
+#     SRC_DIR = f'data/images.nosync/Chest-RSNA/test/{GOOD_UNGOOD}'
+#     TRAIN_DIR = f'data/output.nosync/Chest-RSNA/train_512_512_1000_1/{GOOD_UNGOOD}' # will be same as SAVE_DIR if used after VisibilityDetection
+#     TEST_DIR = 'data/output.nosync/Chest-RSNA/train_512_512_1000_1/good'
+#     PKL_DUMP_PATH = f'data/pickles/'
+#     PKL_FILE_NAME = f'{TEST_OR_TRAIN}_{IMG_HEIGHT}_{IMG_WIDTH}_{A0}_{A1}_{GOOD_UNGOOD}_hundred'
+#     REORDERED_PKL_FILE_NAME = f'{TEST_OR_TRAIN}_{IMG_HEIGHT}_{IMG_WIDTH}_{A0}_{A1}_{GOOD_UNGOOD}_hundred_reordered'
+#     EIGEN_PKL_FILE_NAME = f'{TEST_OR_TRAIN}_{IMG_HEIGHT}_{IMG_WIDTH}_{A0}_{A1}_{GOOD_UNGOOD}_hundred_eigen_NEW'
+#     K_TRAIN = 3
+#     K_TEST = 3
+#     # chest_train_img_processor = img_processing.VisibilityDetection(SRC_DIR, img_height=IMG_HEIGHT, img_width=IMG_WIDTH, a0=A0, a1=A1, save=SAVE_DIR)
+#     # chest_train_img_processor.run()
+    
+    
+#     # ''' TRAIN '''
+#     chest_train_model = model.Model(IMG_HEIGHT, IMG_WIDTH, train_dir=TRAIN_DIR)
+#     # chest_train_model.extract_features(PKL_DUMP_PATH, PKL_FILE_NAME, concurrent=True)
+#     # chest_train_model.load_features(f'{PKL_DUMP_PATH}/{PKL_FILE_NAME}')
+#     # chest_train_model.reorder_features(PKL_DUMP_PATH, REORDERED_PKL_FILE_NAME, k=K)
+#     chest_train_model.load_reordered_features(f'{PKL_DUMP_PATH}/{REORDERED_PKL_FILE_NAME}')
+#     chest_train_model.knnn(PKL_DUMP_PATH, EIGEN_PKL_FILE_NAME, mode='train', k=K_TRAIN)
+#     chest_train_model.load_eigen_mem(f'{PKL_DUMP_PATH}/{EIGEN_PKL_FILE_NAME}')
+
+
+#     # ''' TEST '''
+#     # chest_train_model = model.Model(IMG_HEIGHT, IMG_WIDTH, train_dir=None, test_dir=TEST_DIR)
+#     # chest_train_model.extract_features(PKL_DUMP_PATH, PKL_FILE_NAME, concurrent=True)
+#     # chest_train_model.load_features(f'{PKL_DUMP_PATH}/{PKL_FILE_NAME}')
+#     # chest_train_model.reorder_features(PKL_DUMP_PATH, REORDERED_PKL_FILE_NAME, k=K)
+#     # chest_train_model.load_reordered_features(f'{PKL_DUMP_PATH}/{REORDERED_PKL_FILE_NAME}')
+#     # chest_train_model.load_eigen_mem(f'{PKL_DUMP_PATH}/{EIGEN_PKL_FILE_NAME}')
+#     # chest_train_model.knnn(PKL_DUMP_PATH, EIGEN_PKL_FILE_NAME, mode='test', k=K_TEST)
+    
+# driver()
+
+
 import img_processing
 import model
 import numpy as np
@@ -10,28 +64,54 @@ def driver():
     '''
 
 
-    TEST_OR_TRAIN = 'train' # 'train'
+    TEST_OR_TRAIN = 'test' # 'train'
     GOOD_UNGOOD = 'good'
     IMG_HEIGHT = 512
     IMG_WIDTH = 512
     A0 = 1000
     A1 = 1
+    K_TRAIN = 25
+    K_TEST = 15
+    SET_SIZE = 3
+
+
+
+    TRAIN_NUM_IMGS = 100
     SAVE_DIR = f'data/output.nosync/Chest-RSNA/{TEST_OR_TRAIN}_{IMG_HEIGHT}_{IMG_WIDTH}_{A0}_{A1}/{GOOD_UNGOOD}' # False
-    SRC_DIR = f'data/images.nosync/Chest-RSNA/val/{GOOD_UNGOOD}'
+    SRC_DIR = f'data/images.nosync/Chest-RSNA/{TEST_OR_TRAIN}/{GOOD_UNGOOD}'
     TRAIN_DIR = f'data/output.nosync/Chest-RSNA/train_512_512_1000_1/{GOOD_UNGOOD}' # will be same as SAVE_DIR if used after VisibilityDetection
+    TEST_DIR = 'data/output.nosync/Chest-RSNA/train_512_512_1000_1/good'
     PKL_DUMP_PATH = f'data/pickles/'
-    PKL_FILE_NAME = f'{TEST_OR_TRAIN}_{IMG_HEIGHT}_{IMG_WIDTH}_{A0}_{A1}_{GOOD_UNGOOD}_hundred'
-    EIGEN_PKL_FILE_NAME = f'{TEST_OR_TRAIN}_{IMG_HEIGHT}_{IMG_WIDTH}_{A0}_{A1}_{GOOD_UNGOOD}_hundred_eigen'
-    K = 3
+    FEATURES_PKL_FILE_NAME = f'{TEST_OR_TRAIN}_{IMG_HEIGHT}_{IMG_WIDTH}_{A0}_{A1}_{GOOD_UNGOOD}_{TRAIN_NUM_IMGS}'
+    REORDERED_PKL_FILE_NAME = f'{TEST_OR_TRAIN}_{IMG_HEIGHT}_{IMG_WIDTH}_{A0}_{A1}_{GOOD_UNGOOD}_{TRAIN_NUM_IMGS}_{SET_SIZE}_reordered'
+    EIGEN_PKL_FILE_NAME = f'{TEST_OR_TRAIN}_{IMG_HEIGHT}_{IMG_WIDTH}_{A0}_{A1}_{GOOD_UNGOOD}_{TRAIN_NUM_IMGS}_{SET_SIZE}_{K_TRAIN}_eigen'
 
     # chest_train_img_processor = img_processing.VisibilityDetection(SRC_DIR, img_height=IMG_HEIGHT, img_width=IMG_WIDTH, a0=A0, a1=A1, save=SAVE_DIR)
     # chest_train_img_processor.run()
 
-    chest_train_model = model.Model(IMG_HEIGHT, IMG_WIDTH, train_dir=TRAIN_DIR)
-    chest_train_model.extract_features(PKL_DUMP_PATH, PKL_FILE_NAME, concurrent=True)
-    chest_train_model.load_features(f'{PKL_DUMP_PATH}/{PKL_FILE_NAME}')
-    chest_train_model.reorder_features(k=K)
-    chest_train_model.knnn(PKL_DUMP_PATH, EIGEN_PKL_FILE_NAME, k=K)
-    chest_train_model.load_eigen_mem(f'{PKL_DUMP_PATH}/{EIGEN_PKL_FILE_NAME}')
+    chest_train_model = model.Model(IMG_HEIGHT, IMG_WIDTH, train_dir='/Users/kevingomez/Desktop/CS670/CS670_Project/PyMAD/data/output.nosync/Chest-RSNA/train_HQ/good')
+    chest_train_model.extract_features(PKL_DUMP_PATH, 'train_HQ', concurrent=True, select_images=8000)
 
+    chest_train_model = model.Model(IMG_HEIGHT, IMG_WIDTH, train_dir='/Users/kevingomez/Desktop/CS670/CS670_Project/PyMAD/data/output.nosync/Chest-RSNA/test_HQ/good')
+    chest_train_model.extract_features(PKL_DUMP_PATH, 'test_good_100_HQ', concurrent=True, select_images=TRAIN_NUM_IMGS)
+
+    chest_train_model = model.Model(IMG_HEIGHT, IMG_WIDTH, train_dir='/Users/kevingomez/Desktop/CS670/CS670_Project/PyMAD/data/output.nosync/Chest-RSNA/test_HQ/ungood')
+    chest_train_model.extract_features(PKL_DUMP_PATH, 'test_ungood_100_HQ', concurrent=True, select_images=TRAIN_NUM_IMGS)
+
+    # ''' TRAIN '''
+    # chest_train_model = model.Model(IMG_HEIGHT, IMG_WIDTH, train_dir=TRAIN_DIR)
+    # chest_train_model.extract_features(PKL_DUMP_PATH, FEATURES_PKL_FILE_NAME, concurrent=True, select_images=TRAIN_NUM_IMGS)
+    # chest_train_model.load_features(f'{PKL_DUMP_PATH}/{FEATURES_PKL_FILE_NAME}')
+    # chest_train_model.reorder_features(PKL_DUMP_PATH, REORDERED_PKL_FILE_NAME, set_size=SET_SIZE)
+    # chest_train_model.load_reordered_features(f'{PKL_DUMP_PATH}/{REORDERED_PKL_FILE_NAME}')
+    # chest_train_model.knnn(PKL_DUMP_PATH, EIGEN_PKL_FILE_NAME, mode='train', k=K_TRAIN, set_size=SET_SIZE)
+    # chest_train_model.load_eigen_mem(f'{PKL_DUMP_PATH}/{EIGEN_PKL_FILE_NAME}')
+
+
+    # ''' TEST '''
+    # chest_train_model = model.Model(IMG_HEIGHT, IMG_WIDTH, train_dir=None, test_dir=TEST_DIR)
+    # chest_train_model.load_reordered_features(f'{PKL_DUMP_PATH}/{REORDERED_PKL_FILE_NAME}')
+    # chest_train_model.load_eigen_mem(f'{PKL_DUMP_PATH}/{EIGEN_PKL_FILE_NAME}')
+    # chest_train_model.knnn(PKL_DUMP_PATH, EIGEN_PKL_FILE_NAME, mode='test', k=K_TEST, set_size=SET_SIZE)
+    
 driver()
--- src/model.py
+++ src/model.py
@@ -15,6 +15,8 @@ import math
 import concurrent.futures as cf
 from sklearn.neighbors import NearestNeighbors
 from numpy import linalg as LA
+from tqdm import tqdm
+import cv2
 
 class Model:
     def __init__(self, transform_height, transform_width, train_dir=None, test_dir=None, save=None) -> None:
@@ -56,7 +58,8 @@ class Model:
                 T.Resize(size=(self.patch_based_height, self.patch_based_width)),
                 T.CenterCrop(size=(self.patch_based_height, self.patch_based_width)),
                 T.ToTensor(),
-                T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+                # T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+                T.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
                 ]
             )
         }
@@ -93,74 +96,6 @@ class Model:
             print(f"!!! Error in _type_check: \n {e}")
         return False
     
-    def calculate_accuracy(self, outputs, labels):
-        # Convert outputs to probabilities using sigmoid
-        probabilities = torch.sigmoid(outputs)
-        # Convert probabilities to predicted classes
-        predicted_classes = probabilities > 0.5
-        # Calculate accuracy
-        correct_predictions = (predicted_classes == labels.byte()).sum().item()
-        total_predictions = labels.size(0)
-        return correct_predictions / total_predictions
-
-    def train(self):
-        if not self._type_check('train_dir', self.train_dir, str):
-            raise TypeError
-        try:
-            image_datasets = {
-                "train": datasets.ImageFolder(self.train_dir, self.img_transform["train"])
-            }
-            dataloaders = {
-                "train": torch.utils.data.DataLoader(image_datasets["train"], batch_size=8, shuffle=True)
-            }
-            class_names = image_datasets["train"].classes
-            print(class_names)
-        except Exception as e:
-            print('Error loading images in train')
-            raise e
-        
-        # model = DinoVisionTransformerClassifier("base")
-        model = torch.hub.load("facebookresearch/dinov2", "dinov2_vits14")
-        model = model.to(self.device)
-        model = model.train()
-        criterion = nn.BCEWithLogitsLoss()
-        optimizer = optim.Adam(model.parameters(), lr=1e-6)
-        num_epochs = 50
-        epoch_losses = []
-        epoch_accuracies = []
-
-        print("Training...")
-        for epoch in range(num_epochs):
-            batch_losses = []
-            batch_accuracies = []
-
-            for data in dataloaders["train"]:
-                # get the input batch and the labels
-                batch_of_images, labels = data
-
-                # zero the parameter gradients
-                optimizer.zero_grad()
-
-                # model prediction
-                output = model(batch_of_images.to(self.device))
-                output = output.squeeze(dim=1)
-                # compute loss and do gradient descent
-                loss = criterion(output, labels.unsqueeze(1).float().to(self.device))
-
-                loss.backward()
-                optimizer.step()
-
-                batch_losses.append(loss.item())
-                
-                # Calculate and record batch accuracy
-                accuracy = self.calculate_accuracy(output, labels.to(self.device))
-                batch_accuracies.append(accuracy)
-
-            epoch_losses.append(np.mean(batch_losses))
-            epoch_accuracy = np.mean(batch_accuracies)
-            epoch_accuracies.append(epoch_accuracy)
-            print("  -> Epoch {}: Loss = {:.5f}, Accuracy = {:.3f}%".format(epoch, epoch_losses[-1], 100*epoch_accuracy))
-
 
     def _extract_features(self, image):
         if type(image) is str:
@@ -169,6 +104,13 @@ class Model:
         else:
             image.data = Image.open(image.path).convert('RGB')
             img = self.img_transform['embedding'](image.data).unsqueeze(0)
+            # x = self.img_transform['embedding'](image.data)
+            # x = x.numpy()
+            # x = np.transpose(x, (1, 2, 0))
+            # cv2.imshow('im', x)
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
+            # quit()
         with torch.no_grad():
             features = self.dino(img)
             # features = self.dino.forward_features(img)["x_norm_patchtokens"]
@@ -177,36 +119,59 @@ class Model:
         return image.path, features
         
 
-    def extract_features(self, pkl_dump_path, pkl_file_name, concurrent=True):
+    def extract_features(self, pkl_dump_path, pkl_file_name, concurrent=True, select_images=None):
+        assert len(self.images.images) != 0, 'No images!'
+        num_images = len(self.images.images)
+        assert select_images is None \
+            or (type(select_images) is int and select_images > 0 and select_images <= num_images), 'Invalid select_images val'
+        if select_images is not None:
+            num_images = select_images
         self.dino.eval()
         if concurrent:
             with cf.ThreadPoolExecutor() as executor:
-                future_to_image = {executor.submit(self._extract_features, image): image for image in self.images.images}
-                for future in cf.as_completed(future_to_image):
+                future_to_image = {executor.submit(self._extract_features, image): image for image in self.images.images[:num_images]}
+                for future in tqdm(cf.as_completed(future_to_image), total=len(future_to_image), desc='Extracting img features'):
                     image_path, features = future.result()
-                    print(image_path)
-                    self.extracted_features.append((image_path, features))
+                    # print(image_path)
+                    self.extracted_features.append(features.view(-1).numpy())
         else:
             for image in self.images.images:
                 image_path, features = self._extract_features(image)
                 print(image_path, features.shape)
                 self.extracted_features.append((image_path, features))
+        self.extracted_features = np.array(self.extracted_features)
         self.file_handler.dump_data(self.extracted_features, pkl_dump_path, pkl_file_name)
         return self.extracted_features
     
+    def compute_correlation_matrix(self, data):
+        """
+        Computes the correlation matrix for a given 2D NumPy array.
+        
+        :param data: A 2D NumPy array of size [n, m] where n is the number of samples
+                    and m is the number of features.
+        :return: The correlation matrix of size [m, m].
+        """
+        # Standardize each feature (column) to have mean 0 and variance 1
+        standardized_data = (data - np.mean(data, axis=0)) / np.std(data, axis=0, ddof=1)
+
+        # Compute the correlation matrix
+        correlation_matrix = np.dot(standardized_data.T, standardized_data) / (data.shape[0] - 1)
+
+        return correlation_matrix
     
-    def reorder_features(self, pkl_dump_path, pkl_file_name, k=3):
+    def reorder_features(self, pkl_dump_path, pkl_file_name, set_size=3):
         assert len(self.extracted_features)!=0 , 'Load features first!'
 
         ''' v's original shape = (1, (self.patch_h)^2, feat_dim) if using forward_features to extract features
             v's original shape = (1, feat_dim) if not
             below flattens to 1369*384 or (self.patch_h)^2*feat_dim if using forward_features to extract features
-            below flattens to 1369*384 or (self.patch_h)^2*feat_dim if using forward_features to extract features'''
-        feat_mat = np.array([v.view(-1).numpy() for (p,v) in self.extracted_features]) 
+            below flattens to 384 or feat_dim if not'''
+        feat_mat = self.extracted_features
         N = self.feat_dim # dim of test point feature f (same for all points)
-        S = N//k # divide into S sets
-        L = k # dim of subfeature vectors. So more samples used to calculate the Eigen vectors = larger L
+        S = N//set_size # divide into S sets
+        L = set_size # dim of subfeature vectors. So more samples used to calculate the Eigen vectors = larger L
         corr_matrix = np.corrcoef(feat_mat, rowvar=False) # find correlation between all pairs in training set
+        # correlation_matrix = self.compute_correlation_matrix(feat_mat) # verified returns the same as above
         reordered_indices = list()
         curr_ind = 0
         set_num = 0
@@ -252,13 +217,19 @@ class Model:
         set_matrices = [neighbor_features_mat[:, i*L:(i+1)*L] for i in range(S)]
         all_eigenvals = list()
         all_eigenvects = list()
-        # c = 0
         for submatrix in set_matrices:
-            eigenvalues, eigenvectors = LA.eig(submatrix) # normalized eigenvectors (each column)
-            # if np.any(eigenvalues < 0):
-            #     c+=1
+            cov_matrix = np.cov(submatrix, rowvar=False)
+            try:
+                eigenvalues, eigenvectors = LA.eig(cov_matrix)  # normalized eigenvectors (each column)
+            except:
+                print("Eigenvector calc exception")
+                # If the covariance matrix is singular, use just an identity matrix
+                eigenvalues = np.ones(cov_matrix.shape[0])
+                eigenvectors = np.eye(cov_matrix.shape[0])
+
             all_eigenvals.append(eigenvalues)
             all_eigenvects.append(eigenvectors)
+            # print(eigenvalues)
         return (all_eigenvals, all_eigenvects)
 
     def calc_anomaly_score(self, f, neighbor_features_mat, eigen_mem, S, L, k=3):
@@ -266,7 +237,7 @@ class Model:
         f_sets = [f[i*L:(i+1)*L] for i in range(S)]
         score = 0
         for i in range(k):
-            for j in range(k):
+            for j in range(L):
                 for s in range(S):
                     # print(len(eigen_mem[i][0]))
                     # print(len(eigen_mem[i][1]))
@@ -275,18 +246,24 @@ class Model:
                     # print(neigh_sets[s][i])
                     # print(f_sets[s] - neigh_sets[s][i])
                     a = np.abs(np.dot(f_sets[s] - neigh_sets[s][i], eigen_mem[i][1][s][j]))
+                    assert eigen_mem[i][0][s][j] > 0, f'Negative or zero eigenvalue! {eigen_mem[i][0][s][j]}'
                     b = 1/np.emath.sqrt(eigen_mem[i][0][s][j])
                     score += np.dot(a,b)
+                    # print(a)
+                    # print(b)
                     # print(a,b)
                     # print(score)
-        print(score)
+                    # quit()
+        # quit()
+        return score
 
 
-    def knnn(self, pkl_dump_path, pkl_file_name, mode, k=3, exclude_test_point=True):
+    def knnn(self, pkl_dump_path, pkl_file_name, mode, k, set_size, exclude_test_point=True, test_ds=set()):
         assert self.reordered_feat_mat is not None, 'Reorder features first!'
         N = self.feat_dim # dim of test point feature f (same for all points)
-        S = N//k # divide into S sets
-        L = k # dim of subfeature vectors. So more samples used to calculate the Eigen vectors = larger L
+        S = N//set_size # divide into S sets
+        L = set_size # dim of subfeature vectors. So more samples used to calculate the Eigen vectors = larger L
+        print(f'k:{k}, N: {N}, S:{S}, L:{L}')
         if exclude_test_point and mode == 'train':
             neigh = NearestNeighbors(n_neighbors=k+1)
         else:
@@ -304,13 +281,22 @@ class Model:
 
         elif mode == 'test':
             assert len(self.eigen_memory.keys()) != 0, 'Load eigen memory first!'
-            for image in self.images.images:
+
+            scores = list()
+            # for image in tqdm(self.images.images, desc='Processing images'):
+            for image in self.images.images[:25]:
                 f = self._extract_features(image.path)[1].view(-1).numpy()
                 neighbors = neigh.kneighbors([f])
                 neighbor_indices = neighbors[1][0]
                 neighbor_features_mat = self.reordered_feat_mat[neighbor_indices] 
                 req_eigen_mem = {i: self.eigen_memory[key] for i, key in enumerate(neighbor_indices)}
-                self.calc_anomaly_score(f, neighbor_features_mat, req_eigen_mem, S, L, k=k)
+                score = self.calc_anomaly_score(f, neighbor_features_mat, req_eigen_mem, S, L, k=k)
+                print(score)
+                scores.append(score)
+            avg_score = np.mean(scores)
+            print('AVG score:', avg_score)
+            print('Min score:', np.min(scores))
+            print('Max score:', np.max(scores))
 
         else:
             print(f'Invalid value for mode: {mode}')
